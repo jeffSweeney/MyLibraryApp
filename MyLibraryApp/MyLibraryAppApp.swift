e3a92b3c92f4010ca807709714877d69bb088af7
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-21 15:18:13 -0400

    Initial Commit

---
+++ MyLibraryApp/MyLibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryAppApp.swift
+//  MyLibraryApp
+//
+//  Created by Jeffrey Sweeney on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
