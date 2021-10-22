e495381c6a989087cf3679082300fb7ff03f7990
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-22 13:40:30 -0400

    Implement the book view

--- MyLibraryApp/Views/BookView.swift
+++ MyLibraryApp/Views/BookView.swift
@@ -8,10 +8,63 @@
 import SwiftUI
 
 struct BookView: View {
-    let book: Book
+    @EnvironmentObject var model: BookModel
+    
+    // Default state property. Will read official value onAppear.
+    @State var ratingIndex = 2
+    
+    var book: Book
     
     var body: some View {
-        Text("IMPLEMENT - BookView - Book is \(book.title)")
+        VStack (alignment: .center) {
+            Text("Read Now!")
+                .font(.title)
+                .padding(.top, 30)
+            
+            NavigationLink(destination: {ReadingView(book: book)}) {
+                Image("cover\(book.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            
+            VStack (spacing: 10) {
+                Text("Mark for later!")
+                    .font(.headline)
+                
+                Button(action: {
+                    model.flipFavorite(forId: book.id)
+                }) {
+                    let sfSymbol = book.isFavorite ? "star.fill" : "star"
+                    Image(systemName: sfSymbol)
+                        .resizable()
+                        .scaledToFill()
+                        .accentColor(.yellow)
+                        .frame(width: 20, height: 20)
+                }
+            }
+            
+            VStack {
+                Text("Rate \(book.title)")
+                    .font(.headline)
+                
+                Picker("Book Rating", selection: $ratingIndex) {
+                    ForEach(1...5, id: \.self) { index in
+                        Text(String(index)).tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal, 40)
+                .onChange(of: ratingIndex) { newRating in
+                    model.updateRating(forId: book.id, newRating: newRating)
+                }
+            }
+            .padding(.vertical, 20)
+        }
+        .navigationTitle(book.title)
+        .onAppear(perform: {
+            ratingIndex = book.rating
+        })
     }
 }
 
@@ -19,5 +72,6 @@ struct BookView_Previews: PreviewProvider {
     static var previews: some View {
         let model = BookModel.instance
         BookView(book: model.myBooks[0])
+            .environmentObject(model)
     }
 }
